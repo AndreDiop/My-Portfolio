b851e957a6e9d7bdb8d57a654580c2d5a3ec5b21
Andre Diop <itsjustandre@gmail.com>
2021-02-18 16:44:28 -0500

    Imported containers into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from "react";
+import About from "./containers/About";
+import Portfolio from "./containers/Portfolio";
+import Contact from "./containers/Contact";
+
 function App() {
   return (
     <>
       <p>Hello world</p>
+      <About />
+      <Portfolio />
+      <Contact />
     </>
   );
 }
