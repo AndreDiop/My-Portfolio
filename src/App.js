cdfae69aa732e090d79aa0a0226fb724693937f6
Andre Diop <itsjustandre@gmail.com>
2021-02-19 19:29:21 -0500

    Application overhaul
    - An instance of react was created in my repository and I had to delete the uneeded files and port the neccessary files over

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./containers/About";
+import Portfolio from "./containers/Portfolio";
+import Contact from "./containers/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <nav className="level">
+        <p className="level-item has-text-centered">
+          <a className="link is-info">Home</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p className="level-item has-text-centered">
+          <a className="link is-info">Menu</a>
+        </p>
+        <p className="level-item has-text-centered">
+          <p>Andre Diop</p>
+        </p>
+        <p className="level-item has-text-centered">
+          <a className="link is-info">Reservations</a>
+        </p>
+        <p className="level-item has-text-centered">
+          <a className="link is-info">Contact</a>
+        </p>
+      </nav>
+      <p>Hello world</p>
+      <About />
+      <Portfolio />
+      <Contact />
+    </>
   );
 }
 
