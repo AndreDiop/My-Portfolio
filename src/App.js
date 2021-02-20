b72a87b540276195a9e17dc7d4a54c935612fc63
Andre Diop <itsjustandre@gmail.com>
2021-02-19 22:17:45 -0500

    About me section
    - Creating an about me section with info and a gif picture of me

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Contact from "./containers/Contact";
 function App() {
   return (
     <>
-      <div className="nes-container is-rounded">
-        <h1>Andre Diop</h1>
+      <div className="nes-container is-rounded with-title is-centered">
+        <div className="title">
+          <h1>
+            Andre Diop
+            <p>Full Stack Web Developer</p>
+          </h1>
+        </div>
         <a className="nes-btn" href="#">
           About
         </a>
@@ -32,10 +37,28 @@ function App() {
         </a>
       </div>
 
-      <p>Hello world</p>
-      <About />
-      <Portfolio />
-      <Contact />
+      <div className="nes-container is-rounded">
+        <About />
+        <section className="message -left">
+          <div className="nes-balloon ">
+            <p>
+              My name is Andre Diop and I am an Atlanta based full stack web
+              developer responsible for all parts of the development lifecycle.
+            </p>
+          </div>
+          <div className="nes-balloon from-left">
+            <p>
+            Aside from deploying awesomely responsive mobile first websites and applications, I can be found training in Brazilian Jiu Jitsu (I'm a blue belt), playing Settlers of Catan (I'm unstoppable) or trying to master the Aerobie Orbiter (it's amazing!).
+            </p>
+          </div>
+        </section>
+        <img
+          src="https://media.giphy.com/media/7eW3Rof21dV3MZ948r/giphy.gif"
+          width="200"
+          height="200"
+        />
+      </div>
+      
     </>
   );
 }
