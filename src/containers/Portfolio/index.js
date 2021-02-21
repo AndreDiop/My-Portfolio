e0c75918a7f63b6a762320514becfc856033f662
Andre Diop <itsjustandre@gmail.com>
2021-02-21 13:52:13 -0500

    Abstracted Portfolio
    - with hardcoded values

--- src/containers/Portfolio/index.js
+++ src/containers/Portfolio/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-
+import React from "react";
+import Password from "../../assets/passgen.png"
 const index = () => {
-    return (
-        <div>
-            <h1>Portfolio</h1>
-           This is where you will see my work
-        </div>
-    );
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <div className="nes-container is-rounded" id="portfolioContainer">
+        <p className="nes-balloon nes-pointer" id="portfolioCard">
+          <img
+            src={Password}
+            width="200"
+            height="200"
+            id="portfolioImage"
+            alt="Animated gif displaying the functionality of site"
+          />
+          <hr />
+          <p>name here</p>
+          <section />
+          <a
+            className="nes-btn"
+            href="https://github.com/AndreDiop/Password-Generator"
+            id="portfolioRepoButton"
+          >
+            Github Repo
+          </a>
+
+          <a
+            className="nes-btn is-primary"
+            href="https://andrediop.github.io/Password-Generator/"
+            id="portfolioDeployLinkButton"
+          >
+            Deployed Link
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default index;
