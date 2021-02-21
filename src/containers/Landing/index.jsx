59f2aeb2047a362a6d862191c8dc45e0d38e5462
Andre Diop <itsjustandre@gmail.com>
2021-02-21 14:31:19 -0500

    Abstract Landing [age

--- src/containers/Landing/index.jsx
+++ src/containers/Landing/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from 'react'
+import Andre from '../../assets/andreFull.png'
+import Start from '../../assets/pressStart.png'
+import { Link } from 'react-router-dom'
 
 const index = () => {
-    return (
-        <div>
-            <h1>This will be a page that people see upon getting here</h1>
-        </div>
-    );
-};
+  return (
+    <div>
+      <div
+        className='nes-container is-rounded is-dark'
+        id='landingLogoContainer'
+      >
+        <p className='nes-balloon nes-pointer'>
+          <img
+            src={Andre}
+            id='landingLogo'
+            alt='Andre Diop full stack engineer stylized in 8-bit'
+          />
+          <section />
+          <Link to='/About'>
+            <img src={Start} id='pressStart' alt='Press start text' />
+          </Link>
+        </p>
+      </div>
+    </div>
+  )
+}
 
-export default index;
+export default index
