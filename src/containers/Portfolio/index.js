51d8b8273fb0e842a6b69e9dc13b55e8d439c302
Andre Diop <itsjustandre@gmail.com>
2021-02-21 16:23:31 -0500

    Now a container for Portfolio Card

--- src/containers/Portfolio/index.js
+++ src/containers/Portfolio/index.js
@@ -1,37 +1,14 @@
 import React from "react";
-import Password from "../../assets/passgen.png"
+import PortfolioCard from "../PortfolioCard";
+import Works from "../../assets/data.json";
 const index = () => {
   return (
     <div>
       <div className="nes-container is-rounded" id="portfolioContainer">
-      <h1>Portfolio</h1>
-        <p className="nes-balloon nes-pointer" id="portfolioCard">
-          <p>name here</p>
-          <img
-            src={Password}
-            width="200"
-            height="200"
-            id="portfolioImage"
-            alt="Animated gif displaying the functionality of site"
-          />
-          <br />
-          <hr />
-          <a
-            className="nes-btn"
-            href="https://github.com/AndreDiop/Password-Generator"
-            id="portfolioRepoButton"
-          >
-            Github Repo
-          </a>
-
-          <a
-            className="nes-btn is-primary"
-            href="https://andrediop.github.io/Password-Generator/"
-            id="portfolioDeployLinkButton"
-          >
-            Deployed Link
-          </a>
-        </p>
+        <h1>Portfolio</h1>
+        {Works.map((data) => (
+          <PortfolioCard key={data.id} {...data} />
+        ))}
       </div>
     </div>
   );
