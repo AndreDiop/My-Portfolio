19389e8b5e4fe028bae6a87b21fea94badc1be54
Andre Diop <itsjustandre@gmail.com>
2021-02-21 17:22:55 -0500

    Changed to hashrouter
    - This should allow it to load on github pages with no problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./containers/Navbar";
 import About from "./containers/About";
 import Portfolio from "./containers/Portfolio";
