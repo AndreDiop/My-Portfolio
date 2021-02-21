a5aefa838691459bc67d36430e3035b47691a485
Andre Diop <itsjustandre@gmail.com>
2021-02-21 13:51:25 -0500

    Abstracted skills
    - Has a container, I may think about abstracting the container later

--- src/containers/Skills/index.js
+++ src/containers/Skills/index.js
@@ -3,7 +3,22 @@ import React from 'react';
 const index = () => {
     return (
         <div>
-            <p>Skills will pay the bills</p>
+             <div
+            className="nes-container with-title is-rounded"
+            id="skillsContainer"
+          >
+            <p className="title" id="skillsHeader">
+              <h1>Skills</h1>
+            </p>
+            <p>Here you will see icons for stuff</p>
+            {/* You can map over these iconse by changing after the fa- */}
+            <i className="fab fa-js-square fa-5x"></i>
+            <i className="fab fa-react fa-5x"></i>
+            <i className="fab fa-npm fa-5x"></i>
+            <i className="fab fa-node fa-5x"></i>
+            <i className="fab fa-css3-alt fa-5x"></i>
+            <i className="fab fa-html5-alt fa-5x"></i>
+          </div>
         </div>
     );
 };
