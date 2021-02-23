681b9c6d2d876fb059f6837f8c110d5db5bff13d
Andre Diop <itsjustandre@gmail.com>
2021-02-22 23:54:46 -0500

    Updated contact statement

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -5,11 +5,10 @@ const index = () => {
     <div className="nes-container is-rounded" id="contactContainer">
       <h1>Contact</h1>
       <p className="nes-balloon nes-pointer">
-        ANDRE DIOP <br />
-        ItsJustAndre@gmail.com <br />
-        (678) 310 - 5409
-        <br />
-        Atlanta Georgia
+        I have an eye for design and a passion for user crafting an exceptional
+        user experience which influences my professional work, as well as my
+        side projects. Feel free to contact me through the links below and
+        experience how my skills can be applied to your next project.
       </p>
     </div>
   );
