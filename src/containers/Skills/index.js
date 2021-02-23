a64079cd9e9477b6a72f13796fdc43a130a7fe65
Andre Diop <itsjustandre@gmail.com>
2021-02-22 22:24:16 -0500

    Centered container
    - dynamically generated icons
    - centered content

--- src/containers/Skills/index.js
+++ src/containers/Skills/index.js
@@ -1,19 +1,20 @@
 import React from "react";
-
+import "../Skills/styles.css";
+import Icons from "../../assets/data.json";
+import Icon from "../Icon";
 const index = () => {
+  console.log(Icons);
   return (
     <div>
       <div className="nes-container is-rounded" id="skillsContainer">
         <h1>Skills</h1>
 
-        <p>Here you will see icons for stuff</p>
-        {/* You can map over these iconse by changing after the fa- */}
-        <i className="fab fa-js-square fa-5x"></i>
-        <i className="fab fa-react fa-5x"></i>
-        <i className="fab fa-npm fa-5x"></i>
-        <i className="fab fa-node fa-5x"></i>
-        <i className="fab fa-css3-alt fa-5x"></i>
-        <i className="fab fa-html5-alt fa-5x"></i>
+        <p>Languages and frameworks that I use</p>
+
+        {Icons.map((icon) =>(
+          <Icon {...icon} />
+          
+        ))}
       </div>
     </div>
   );
