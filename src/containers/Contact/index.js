0fc0cee0422d06c0f1f11d0905503c7079392cb9
Andre Diop <itsjustandre@gmail.com>
2021-02-23 09:34:21 -0500

    Typo

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -5,8 +5,8 @@ const index = () => {
     <div className="nes-container is-rounded" id="contactContainer">
       <h1>Contact</h1>
       <p className="nes-balloon nes-pointer">
-        I have an eye for design and a passion for user crafting an exceptional
-        user experience which influences my professional work, as well as my
+        I have an eye for design and I truly enjoy curating an exceptional
+        user experience which influences my professional work as well as my
         side projects. Feel free to contact me through the links below and
         experience how my skills can be applied to your next project.
       </p>
